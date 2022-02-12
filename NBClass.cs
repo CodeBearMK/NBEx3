2e0e1193c9f290ef7357bcbfdaa22a93f05dbfe4
cat32tw <mike1993xd@gmail.com>
2022-02-13 00:12:26 +0800

    類別練習

---
+++ NBClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBEx3
+{
+    internal class NBClass
+    {
+        private int price;
+        
+        public void setPrice(int tPrice)
+        {
+            price = tPrice;
+        }
+        public int getPrice()
+        {
+            return price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NBEx3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NBClass sun = new NBClass();
+            sun.setPrice(25000);
+            Console.WriteLine("Sun筆電單價{0}", sun.getPrice());
+            Console.Read();
+        }
+    }
+}
